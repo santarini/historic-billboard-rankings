e2430b6945529a2ba35baf3b079091de824c7ad1
Makoa <32736507+santarini@users.noreply.github.com>
2018-04-13 13:50:56 -1000

    Update dateFinal.py

--- dateFinal.py
+++ dateFinal.py
@@ -10,12 +10,13 @@ with open("billboardDateHrefs.csv") as csvfileA:
     with open('dateRank.csv', 'a') as csvfileB:
         fieldnames = ['Year','IssueDate','Rank', 'Artist', 'Song']
         writer = csv.DictWriter(csvfileB, fieldnames=fieldnames, lineterminator = '\n')
-        writer.writeheader()
         for row in reader:
             year = (row['Year'])
             dateText = (row['IssueDate'])
             href = (row['Href'])
-            response = requests.get('https://www.billboard.com' + href)
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',}
+            url = 'https://www.billboard.com' + href
+            response = requests.get(url, headers=headers)
             soup = bs.BeautifulSoup(response.text, 'lxml')
             chart = soup.findAll("div", {"class": "container"})[2]
             for row in chart.findAll('article'):
@@ -27,7 +28,6 @@ with open("billboardDateHrefs.csv") as csvfileA:
                 except(TypeError, AttributeError):
                     artistName = row.find("span", {"class": "chart-row__artist"}).text
                     artistName = artistName.strip()
-
                 writer.writerow({'Year': year,'IssueDate':dateText,'Rank':weekRank, 'Artist':artistName,'Song':songName})
                 print(dateText+", "+ year+": "+weekRank +", " + artistName +" - "+songName)
 elapsed_time = time.time() - start_time
